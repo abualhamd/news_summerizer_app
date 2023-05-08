7a36af91cf38bce8114efbfa91319e8d1cff1d26
Muhammad Abu-Alhamd <53705516+abualhamd@users.noreply.github.com>
2023-05-08 10:17:33 +0300

    initial commit

--- android/app/src/main/kotlin/com/example/news_summerizer_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/news_summerizer_app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.news_app
+package com.example.news_summerizer_app
 
 import io.flutter.embedding.android.FlutterActivity
 
--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\Users\benha\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\flutter_apps\Abdallah_Mansour\news_app"
+export "FLUTTER_APPLICATION_PATH=D:\flutter_apps\news_summerizer_app"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
